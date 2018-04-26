b7e8eeae86ecb75490b6edb4f2d4cf933462a7eb
Zhang Chi <624593758@qq.com>
2018-04-26 09:46:33 +0800

    vue-music first commit

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
+import 'common/stylus/index.styl'
+
+/* 消除移动端点击300ms延迟 */
+fastclick.attach(document.body)
+
+/* vue懒加载 */
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  store,
+  router
+})
